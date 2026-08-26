51a34c8f5948e885b0ee36d34af40cc5419f3a71
pouria80 <98477450+pouria80@users.noreply.github.com>
2026-08-26 04:01:23 +0330

    feat: add standalone customer dashboard package to main

---
+++ customer-dashboard-react-standalone/src/App.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { I18nProvider } from './i18n';
+import CustomerDashboard from './pages/CustomerDashboard';
+export default function App(){return <I18nProvider><CustomerDashboard/></I18nProvider>}

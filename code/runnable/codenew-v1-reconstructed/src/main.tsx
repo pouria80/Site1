b2111c3f466d57ab0c853121697874fe0cb453c2
pouria80 <98477450+pouria80@users.noreply.github.com>
2026-08-26 19:21:18 +0330

    feat: add codenew v1 bootstrap

---
+++ code/runnable/codenew-v1-reconstructed/src/main.tsx
@@ -0,0 +1,1 @@
+import React from 'react';import{createRoot}from'react-dom/client';import CustomerHub from './pages/CustomerHub';import'./styles/hub.css';createRoot(document.getElementById('root')!).render(<React.StrictMode><CustomerHub/></React.StrictMode>);
